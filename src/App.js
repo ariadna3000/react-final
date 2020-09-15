3bd66fabda25b71134414d844058585d658ff97e
ariadna3000 <ariadna.chalo@gmail.com>
2020-09-15 22:38:33 +0200

    Fix error

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
        
-        <Weather DefaultCity="Berlin" />
+        <Weather DefaultCity="New York" />
         <footer>
           This project was coded by{" "}
           <a

d5f79556295e02a1e2599754fdba5c23acc2b9ac
ariadna3000 <ariadna.chalo@gmail.com>
2020-09-15 12:32:40 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by <a href="https://www.linkedin.com/in/ariadna-ch%C3%A1vez-l%C3%B3pez-8478135b/"
-          target="_blank">
-          Ariadna Chavez </a>
-          and is {" "}
-         <a href="https://github.com/ariadna3000/react-final"
-        target="_blank"
-        >
-        open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/ariadna-ch%C3%A1vez-l%C3%B3pez-8478135b/"
+            target="_blank"
+          >
+            Ariadna Chavez{" "}
+          </a>
+          and is{" "}
+          <a href="https://github.com/ariadna3000/react-final" target="_blank">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

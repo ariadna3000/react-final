8f6511e2b9756b90b1e8e0ca192243d4ea1fb395
ariadna3000 <ariadna.chalo@gmail.com>
2020-09-15 12:14:39 +0200

    Added the GitHiub URL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-    Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="https://www.linkedin.com/in/ariadna-ch%C3%A1vez-l%C3%B3pez-8478135b/"
+          target="_blank">
+          Ariadna Chavez </a>
+          and is {" "}
+         <a href="https://github.com/ariadna3000/react-final"
+        target="_blank"
+        >
+        open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
 
-export default App;

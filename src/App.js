f6119b5afda42f19376dce0a0e905f06e3b1b0a1
ariadna3000 <ariadna.chalo@gmail.com>
2020-09-20 16:05:12 +0200

    Debugging app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
        
-        <Weather DefaultCity="New York" />
+        <Weather defaultCity="Berlin" />
         <footer>
           This project was coded by{" "}
           <a

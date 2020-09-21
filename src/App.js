4818639b741b74f2a89411f2531b9e5287978752
ariadna3000 <ariadna.chalo@gmail.com>
2020-09-21 08:05:30 +0200

    debug

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-       
         <Weather defaultCity="Berlin" />
         <footer>
           This project was coded by{" "}
           <a
             href="https://www.linkedin.com/in/ariadna-ch%C3%A1vez-l%C3%B3pez-8478135b/"
             target="_blank"
+            rel="noopener noreferrer"
           >
             Ariadna Chavez{" "}
           </a>
           and is{" "}
           <a href="https://github.com/ariadna3000/react-final" target="_blank">
-            open-sourced on GitHub
+            open-sourced on GitHub rel="noopener noreferrer"
           </a>
         </footer>
       </div>
